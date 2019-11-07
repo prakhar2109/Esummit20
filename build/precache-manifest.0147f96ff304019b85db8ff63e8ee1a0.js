ecf80826c0e36745a3d33f4a490344ae58de1703
Tushar Varshney <tvarshney@me.iitr.ac.in>
2019-11-08 02:03:28 +0530

    [feat]: Push build

---
+++ build/precache-manifest.0147f96ff304019b85db8ff63e8ee1a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf35f4ad692bd8f8a39eccf6bd2282ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4efa946d12f2a2116271",
+    "url": "/static/css/main.f85eccbf.chunk.css"
+  },
+  {
+    "revision": "42867ef72108a4e5746d",
+    "url": "/static/js/2.81594c5b.chunk.js"
+  },
+  {
+    "revision": "4efa946d12f2a2116271",
+    "url": "/static/js/main.8f6ba810.chunk.js"
+  },
+  {
+    "revision": "d4aa6f1827604e1691da",
+    "url": "/static/js/runtime-main.cffdc255.js"
+  },
+  {
+    "revision": "97d0cb8927706abe49f9aa4d57762a85",
+    "url": "/static/media/theme.97d0cb89.svg"
+  }
+]);
