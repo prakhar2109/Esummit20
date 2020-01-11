eb8bc1847fc5af9bea4a7b3406b0b76d13d05288
adrijshikhar <adrijshikhar85@gmail.com>
2020-01-11 12:15:25 +0530

    added horizontal scroll arrows

--- src/components/home/sections/Sponsors.js
+++ src/components/home/sections/Sponsors.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import sponsorsHeadingBackground from '../../../assets/sponsors-heading-background.svg'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import linkedinlocal from '../../../assets/sponsors/linkedinlocal.jpg'
 import Dribble from '../../../assets/sponsors/dribble.svg'
 import thecollegefever from '../../../assets/sponsors/Thecollegefever.png'
@@ -9,6 +9,7 @@ import hackerearth from '../../../assets/sponsors/hackerearth.png'
 import Devfolio from '../../../assets/sponsors/devfolio.png'
 import CBlock from '../../../assets/sponsors/CBlock.png'
 import AmarUjala from '../../../assets/sponsors/amarujala.webp'
+import arrow from '../../../assets/arrow.svg'
 
 class Sponsors extends Component {
   render() {
@@ -22,32 +23,57 @@ class Sponsors extends Component {
             id="section-image-heading-background-sponsor"
             alt=""
           />
+          <div className="section-horizontal-navigate-container">
+            <img
+              src={arrow}
+              alt=""
+              className="section-horizontal-navigate-arrow arrow-left"
+            />
+            <img
+              src={arrow}
+              alt=""
+              className="section-horizontal-navigate-arrow arrow-right"
+            />
+          </div>
         </div>
-        <div className="sponsors-section-card-container">
+        <div
+          className="sponsors-section-card-container"
+          data-scroll="horizontal"
+        >
           <div className="sponsors-section-card">
-            <img src={linkedinlocal} alt="" className="sponsors-section-card-image" />
+            <img
+              src={linkedinlocal}
+              alt=""
+              className="sponsors-section-card-image"
+            />
             <div className="sponsors-section-card-content">
-              <div className="sponsors-section-card-heading">LinkedIn Local</div>
+              <div className="sponsors-section-card-heading">
+                LinkedIn Local
+              </div>
             </div>
           </div>
           <div className="sponsors-section-card">
             <img src={Dribble} alt="" className="sponsors-section-card-image" />
             <div className="sponsors-section-card-content">
-              <div className="sponsors-section-card-heading">
-                Dribble
-              </div>
+              <div className="sponsors-section-card-heading">Dribble</div>
             </div>
           </div>
           <div className="sponsors-section-card">
-            <img src={Devfolio} alt="" className="sponsors-section-card-image" />
+            <img
+              src={Devfolio}
+              alt=""
+              className="sponsors-section-card-image"
+            />
             <div className="sponsors-section-card-content">
-              <div className="sponsors-section-card-heading">
-                Devfolio
-              </div>
+              <div className="sponsors-section-card-heading">Devfolio</div>
             </div>
           </div>
           <div className="sponsors-section-card">
-            <img src={AmarUjala} alt="" className="sponsors-section-card-image" />
+            <img
+              src={AmarUjala}
+              alt=""
+              className="sponsors-section-card-image"
+            />
             <div className="sponsors-section-card-content">
               <div className="sponsors-section-card-heading">Amar Ujala</div>
             </div>
@@ -65,19 +91,29 @@ class Sponsors extends Component {
             </div>
           </div>
           <div className="sponsors-section-card">
-            <img src={thecollegefever} alt="" className="sponsors-section-card-image" />
+            <img
+              src={thecollegefever}
+              alt=""
+              className="sponsors-section-card-image"
+            />
             <div className="sponsors-section-card-content">
-              <div className="sponsors-section-card-heading">The College Fever</div>
+              <div className="sponsors-section-card-heading">
+                The College Fever
+              </div>
             </div>
           </div>
           <div className="sponsors-section-card">
-            <img src={hackerearth} alt="" className="sponsors-section-card-image" />
+            <img
+              src={hackerearth}
+              alt=""
+              className="sponsors-section-card-image"
+            />
             <div className="sponsors-section-card-content">
               <div className="sponsors-section-card-heading">HackerEarth</div>
             </div>
           </div>
         </div>
-        <Link to ='/sponsors' className="section-button">
+        <Link to="/sponsors" className="section-button">
           <button>view all sponsors</button>
         </Link>
       </div>
