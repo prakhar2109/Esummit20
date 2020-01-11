73a48a07e29758e5446be43b04fad2b629eee5a2
Gaurav Genani <gaurav_g@ch.iitr.ac.in>
2020-01-11 20:35:15 +0530

    Added antd tabs added sample faq questions

--- src/components/faq/index.js
+++ src/components/faq/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import faqHeadingBackground from '../../assets/faq-heading-background.svg'
+import { Tabs } from 'antd'
+const { TabPane } = Tabs
 
 class FAQs extends Component {
   render() {
@@ -18,6 +20,143 @@ class FAQs extends Component {
           <div className="faq-paragraph">
             Stuck somewhere? We’re here to help you!
           </div>
+          <Tabs defaultActiveKey="1">
+            <TabPane tab="General" key="1">
+              <div className="tab-question-container">
+                <div className="question-container">
+                  <input
+                    type="checkbox"
+                    name="question-item"
+                    className="question-items-checkbox"
+                  />
+                  <div className="question-items-container ">
+                    <div className="question-items-container-question">
+                      What is Esummit?
+                      <div className="plus-container">
+                        <hr className="dash" />
+                        <hr className="dash" />
+                      </div>
+                    </div>
+
+                    <div className="question-items-answer">
+                      ESummit being the flagship event of ECell, is held
+                      annually brings together the academic community, venture
+                      capitalists, new age entrepreneurs and all those
+                      passionate about entrepreneurship to common grounds.
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </TabPane>
+            <TabPane tab="General" key="2">
+              <div className="tab-question-container">
+                <div className="question-container">
+                  <input
+                    type="checkbox"
+                    name="question-item"
+                    className="question-items-checkbox"
+                  />
+                  <div className="question-items-container ">
+                    <div className="question-items-container-question">
+                      What is Esummit?
+                      <div className="plus-container">
+                        <hr className="dash" />
+                        <hr className="dash" />
+                      </div>
+                    </div>
+
+                    <div className="question-items-answer">
+                      ESummit being the flagship event of ECell, is held
+                      annually brings together the academic community, venture
+                      capitalists, new age entrepreneurs and all those
+                      passionate about entrepreneurship to common grounds.
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </TabPane>
+            <TabPane tab="General" key="3">
+              <div className="tab-question-container">
+                <div className="question-container">
+                  <input
+                    type="checkbox"
+                    name="question-item"
+                    className="question-items-checkbox"
+                  />
+                  <div className="question-items-container ">
+                    <div className="question-items-container-question">
+                      What is Esummit?
+                      <div className="plus-container">
+                        <hr className="dash" />
+                        <hr className="dash" />
+                      </div>
+                    </div>
+
+                    <div className="question-items-answer">
+                      ESummit being the flagship event of ECell, is held
+                      annually brings together the academic community, venture
+                      capitalists, new age entrepreneurs and all those
+                      passionate about entrepreneurship to common grounds.
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </TabPane>
+            <TabPane tab="General" key="4">
+              <div className="tab-question-container">
+                <div className="question-container">
+                  <input
+                    type="checkbox"
+                    name="question-item"
+                    className="question-items-checkbox"
+                  />
+                  <div className="question-items-container ">
+                    <div className="question-items-container-question">
+                      What is Esummit?
+                      <div className="plus-container">
+                        <hr className="dash" />
+                        <hr className="dash" />
+                      </div>
+                    </div>
+
+                    <div className="question-items-answer">
+                      ESummit being the flagship event of ECell, is held
+                      annually brings together the academic community, venture
+                      capitalists, new age entrepreneurs and all those
+                      passionate about entrepreneurship to common grounds.
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </TabPane>
+            <TabPane tab="General" key="5">
+              <div className="tab-question-container">
+                <div className="question-container">
+                  <input
+                    type="checkbox"
+                    name="question-item"
+                    className="question-items-checkbox"
+                  />
+                  <div className="question-items-container ">
+                    <div className="question-items-container-question">
+                      What is Esummit?
+                      <div className="plus-container">
+                        <hr className="dash" />
+                        <hr className="dash" />
+                      </div>
+                    </div>
+
+                    <div className="question-items-answer">
+                      ESummit being the flagship event of ECell, is held
+                      annually brings together the academic community, venture
+                      capitalists, new age entrepreneurs and all those
+                      passionate about entrepreneurship to common grounds.
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </TabPane>
+          </Tabs>
         </div>
       </div>
     )
