245e197a4d979f9c38f44e7af91a5ce1a0675e66
adrijshikhar <adrijshikhar85@gmail.com>
2020-01-13 16:15:50 +0530

    added on click movement of horizontal div in landing page

--- src/components/home/sections/Speakers.js
+++ src/components/home/sections/Speakers.js
@@ -1,16 +1,9 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import speakersHeadingBackground from '../../../assets/speakers-heading-background.svg'
-import kamalCard from '../../../assets/kamal.png'
-import ajayCard from '../../../assets/ajay.png'
-import samironCard from '../../../assets/samiron.png'
-import tarunCard from '../../../assets/tarun.png'
 import harshCard from '../../../assets/speakers/harsh-hande.jpeg'
-import gulzarCard from '../../../assets/gulzar.png'
-import sharadCard from '../../../assets/sharad.png'
 import arrow from '../../../assets/arrow.svg'
 import karthik from '../../../assets/speakers/karthik-reddy.jpg'
-import coming from '../../../assets/speakers/download.jpeg'
 import abhishek from '../../../assets/speakers/abhishek-prabhuda.jpeg'
 
 import './speakers.css'
@@ -40,54 +33,64 @@ class Speakers extends Component {
             />
           </div>
         </div>
-        <div
-          className="speakers-section-card-container"
-          data-scroll="horizontal"
-        >
-          <div className="speakers-section-card">
-            <img src={karthik} alt="" className="speakers-section-card-image" />
-            <div className="speakers-section-card-content">
-              <div className="speakers-section-card-heading">Karthik Reddy</div>
-              <div className="speakers-section-card-subheading">
-                Blume Ventures
+        <div className="speakers-section-card-scroll-container">
+          <div
+            className="speakers-section-card-container"
+            data-scroll="horizontal"
+          >
+            <div className="speakers-section-card">
+              <img
+                src={karthik}
+                alt=""
+                className="speakers-section-card-image"
+              />
+              <div className="speakers-section-card-content">
+                <div className="speakers-section-card-heading">
+                  Karthik Reddy
+                </div>
+                <div className="speakers-section-card-subheading">
+                  Blume Ventures
+                </div>
               </div>
             </div>
-          </div>
-          <div className="speakers-section-card">
-            <img
-              src={abhishek}
-              alt=""
-              className="speakers-section-card-image"
-            />
-            <div className="speakers-section-card-content">
-              <div className="speakers-section-card-heading">
-                Abhishek Prabhuda
-              </div>
-              <div className="speakers-section-card-subheading">
-                Co-Head at Delhivery
-              </div>
-            </div>
-          </div>
-          <div className="speakers-section-card">
-            <img
-              src={harshCard}
-              alt=""
-              className="speakers-section-card-image"
-            />
-            <div className="speakers-section-card-content">
-              <div className="speakers-section-card-heading">Harish Hande</div>
-              <div className="speakers-section-card-subheading">
-                CEO and Founder, SELCO India
+            <div className="speakers-section-card">
+              <img
+                src={abhishek}
+                alt=""
+                className="speakers-section-card-image"
+              />
+              <div className="speakers-section-card-content">
+                <div className="speakers-section-card-heading">
+                  Abhishek Prabhuda
+                </div>
+                <div className="speakers-section-card-subheading">
+                  Co-Head at Delhivery
+                </div>
               </div>
             </div>
-          </div>
-          {/* <div className="speakers-section-card">
+            <div className="speakers-section-card">
+              <img
+                src={harshCard}
+                alt=""
+                className="speakers-section-card-image"
+              />
+              <div className="speakers-section-card-content">
+                <div className="speakers-section-card-heading">
+                  Harish Hande
+                </div>
+                <div className="speakers-section-card-subheading">
+                  CEO and Founder, SELCO India
+                </div>
+              </div>
+            </div>
+            {/* <div className="speakers-section-card">
             <img src={coming} alt="" className="speakers-section-card-image" />
             <div className="speakers-section-card-content">
               <div className="speakers-section-card-heading">Coming Soon</div>
               <div className="speakers-section-card-subheading"></div>
             </div>
           </div> */}
+          </div>
         </div>
         <Link to="/speakers" className="section-button">
           <button>view all speakers</button>
--- src/components/home/sections/Sponsors.js
+++ src/components/home/sections/Sponsors.js
@@ -36,80 +36,92 @@ class Sponsors extends Component {
             />
           </div>
         </div>
-        <div
-          className="sponsors-section-card-container"
-          data-scroll="horizontal"
-        >
-          <div className="sponsors-section-card">
-            <img
-              src={linkedinlocal}
-              alt=""
-              className="sponsors-section-card-image"
-            />
-            <div className="sponsors-section-card-content">
-              <div className="sponsors-section-card-heading">
-                LinkedIn Local
+        <div className="sponsors-section-card-scroll-container">
+          <div
+            className="sponsors-section-card-container"
+            data-scroll="horizontal"
+          >
+            <div className="sponsors-section-card">
+              <img
+                src={linkedinlocal}
+                alt=""
+                className="sponsors-section-card-image"
+              />
+              <div className="sponsors-section-card-content">
+                <div className="sponsors-section-card-heading">
+                  LinkedIn Local
+                </div>
               </div>
             </div>
-          </div>
-          <div className="sponsors-section-card">
-            <img src={Dribble} alt="" className="sponsors-section-card-image" />
-            <div className="sponsors-section-card-content">
-              <div className="sponsors-section-card-heading">Dribble</div>
-            </div>
-          </div>
-          <div className="sponsors-section-card">
-            <img
-              src={Devfolio}
-              alt=""
-              className="sponsors-section-card-image"
-            />
-            <div className="sponsors-section-card-content">
-              <div className="sponsors-section-card-heading">Devfolio</div>
-            </div>
-          </div>
-          <div className="sponsors-section-card">
-            <img
-              src={AmarUjala}
-              alt=""
-              className="sponsors-section-card-image"
-            />
-            <div className="sponsors-section-card-content">
-              <div className="sponsors-section-card-heading">Amar Ujala</div>
-            </div>
-          </div>
-          <div className="sponsors-section-card">
-            <img src={tides} alt="" className="sponsors-section-card-image" />
-            <div className="sponsors-section-card-content">
-              <div className="sponsors-section-card-heading">Tides</div>
-            </div>
-          </div>
-          <div className="sponsors-section-card">
-            <img src={CBlock} alt="" className="sponsors-section-card-image" />
-            <div className="sponsors-section-card-content">
-              <div className="sponsors-section-card-heading">Coding Blocks</div>
-            </div>
-          </div>
-          <div className="sponsors-section-card">
-            <img
-              src={thecollegefever}
-              alt=""
-              className="sponsors-section-card-image"
-            />
-            <div className="sponsors-section-card-content">
-              <div className="sponsors-section-card-heading">
-                The College Fever
+            <div className="sponsors-section-card">
+              <img
+                src={Dribble}
+                alt=""
+                className="sponsors-section-card-image"
+              />
+              <div className="sponsors-section-card-content">
+                <div className="sponsors-section-card-heading">Dribble</div>
               </div>
             </div>
-          </div>
-          <div className="sponsors-section-card">
-            <img
-              src={hackerearth}
-              alt=""
-              className="sponsors-section-card-image"
-            />
-            <div className="sponsors-section-card-content">
-              <div className="sponsors-section-card-heading">HackerEarth</div>
+            <div className="sponsors-section-card">
+              <img
+                src={Devfolio}
+                alt=""
+                className="sponsors-section-card-image"
+              />
+              <div className="sponsors-section-card-content">
+                <div className="sponsors-section-card-heading">Devfolio</div>
+              </div>
+            </div>
+            <div className="sponsors-section-card">
+              <img
+                src={AmarUjala}
+                alt=""
+                className="sponsors-section-card-image"
+              />
+              <div className="sponsors-section-card-content">
+                <div className="sponsors-section-card-heading">Amar Ujala</div>
+              </div>
+            </div>
+            <div className="sponsors-section-card">
+              <img src={tides} alt="" className="sponsors-section-card-image" />
+              <div className="sponsors-section-card-content">
+                <div className="sponsors-section-card-heading">Tides</div>
+              </div>
+            </div>
+            <div className="sponsors-section-card">
+              <img
+                src={CBlock}
+                alt=""
+                className="sponsors-section-card-image"
+              />
+              <div className="sponsors-section-card-content">
+                <div className="sponsors-section-card-heading">
+                  Coding Blocks
+                </div>
+              </div>
+            </div>
+            <div className="sponsors-section-card">
+              <img
+                src={thecollegefever}
+                alt=""
+                className="sponsors-section-card-image"
+              />
+              <div className="sponsors-section-card-content">
+                <div className="sponsors-section-card-heading">
+                  The College Fever
+                </div>
+              </div>
+            </div>
+            <div className="sponsors-section-card">
+              <img
+                src={hackerearth}
+                alt=""
+                className="sponsors-section-card-image"
+              />
+              <div className="sponsors-section-card-content">
+                <div className="sponsors-section-card-heading">HackerEarth</div>
+              </div>
             </div>
           </div>
         </div>
